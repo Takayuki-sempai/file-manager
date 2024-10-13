192ead8ec49db211c21c4b18a334aba74e3b6c19
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-13 15:38:35 +0300

    feat: add argument parsing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const start = async () => {
-    console.log("Starting project manager...");
+    const usernameArg = process.argv.find((arg) => arg.startsWith("--username"));
+    if(!usernameArg) throw new Error("No username argument provided. Please pass your username in arguments like '--username=your_username'");
+    const username = usernameArg.slice(11);
+    if(!username) throw new Error("Wrong username argument format. Please pass your username in arguments like '--username=your_username'");
+    console.log(`Welcome to the File Manager, ${username}!`);
 }
 
 await start()
