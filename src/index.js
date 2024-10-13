7417bac5e491d31d359582f6bd09537cc8df3f08
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-13 15:46:04 +0300

    fix: change argument parsing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const start = async () => {
-    const usernameArg = process.argv.find((arg) => arg.startsWith("--username"));
-    if(!usernameArg) throw new Error("No username argument provided. Please pass your username in arguments like '--username=your_username'");
-    const username = usernameArg.slice(11);
-    if(!username) throw new Error("Wrong username argument format. Please pass your username in arguments like '--username=your_username'");
+    const username = process.argv.find((arg) => arg.startsWith("--username"))?.slice(11) || "Anonymous"
     console.log(`Welcome to the File Manager, ${username}!`);
 }
 
