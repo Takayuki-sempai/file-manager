13fc540820f354f7aeab393f37212b696a72c753
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-13 21:53:07 +0300

    feat: add goodbye message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+const onUserInput = (chunk) => {
+    const chunkStringified = chunk.toString().trim();
+    switch (chunkStringified) {
+        case '.exit':
+            process.exit(0)
+    }
+    console.log(chunkStringified);
+}
+
 const start = async () => {
     const username = process.argv.find((arg) => arg.startsWith("--username"))?.slice(11) || "Anonymous"
+    process.on('exit', (_) => {
+        console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    });
+    process.on("SIGINT", () => {
+        process.exit();
+    });
     console.log(`Welcome to the File Manager, ${username}!`);
+    process.stdin.on('data', onUserInput);
 }
 
 await start()
